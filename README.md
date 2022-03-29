f411c47e5baa4220260b7291e9407b9e23f8ac19
Sharon Solis <swsolis@ucsb.edu>
2022-03-28 22:07:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cmpsc5a-w22-students
+# This is our class materials for CMPSC 5A (DS 1), Winter 2022 at UC Santa Barbara.
---
+++ hw/hw01/tests/q8_4.py
@@ -0,0 +1,1 @@
+test = {'name': 'q8_4', 'points': None, 'suites': [{'cases': [{'code': '>>> gradescope == 346071\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
