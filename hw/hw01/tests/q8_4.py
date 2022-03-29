416b7bbd5a2ff5e5062f5b775bf075f6581e503b
Sharon Solis <swsolis@ucsb.edu>
2022-03-29 05:29:30 +0000

    removed hws

--- hw/hw01/tests/q8_4.py
+++
@@ -1,1 +0,0 @@
-test = {'name': 'q8_4', 'points': None, 'suites': [{'cases': [{'code': '>>> gradescope == 346071\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
